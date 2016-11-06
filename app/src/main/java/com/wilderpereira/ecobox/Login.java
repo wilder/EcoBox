d02b22d59226aaf3120241aee135326b3c825427
julioc98 <julioc.santos98@gmail.com>
2016-11-06 03:34:37 -0200

    update login

--- app/src/main/java/com/wilderpereira/ecobox/Login.java
+++ app/src/main/java/com/wilderpereira/ecobox/Login.java
@@ -2,6 +2,8 @@ package com.wilderpereira.ecobox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(Login.this,MainActivity.class));
     }
 }
